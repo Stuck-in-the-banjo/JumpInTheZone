210ca61806cb65221aa56fee4a0978483c42327a
Fran <fran152job@gmail.com>
2018-02-01 18:09:03 +0100

    Bounce on walls implemented

---
+++ Mash this jumpo/Assets/Scripts/RightWallScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightWallScript : MonoBehaviour {
+
+    public float force = 10f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+
+        Vector2 knockbackVelocity = new Vector2((transform.position.x - rb.transform.position.x) * force, rb.velocity.y);
+        rb.velocity = knockbackVelocity;
+    }
+}
