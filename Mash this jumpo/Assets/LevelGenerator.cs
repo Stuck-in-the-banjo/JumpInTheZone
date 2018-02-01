80b2324f41c0c2a9a907f23d3a1f2c6b6d321cc9
Fran <fran152job@gmail.com>
2018-02-01 19:09:35 +0100

    Implemented map random generation

---
+++ Mash this jumpo/Assets/LevelGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public GameObject prefabPlatform;
+    public int numberPlatforms = 200;
+    public float levelWidth = 3f;
+    public float minY = 0.2f;
+    public float maxY = 1.5f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Vector3 spawnPos = new Vector3();
+        for(int i = 0; i < numberPlatforms; i++)
+        {
+            spawnPos.y += Random.Range(minY, maxY);
+            spawnPos.x = Random.Range(-levelWidth, levelWidth);
+            Instantiate(prefabPlatform, spawnPos, Quaternion.identity);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
